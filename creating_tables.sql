a5293a171b3b5d88526b47e1b630a64961ee14ce
Daniel Atuesta <danielatuesta55@gmail.com>
2021-05-11 13:39:32 -0500

    uploaded succesfully the data into the data base

--- creating_tables.sql
+++ creating_tables.sql
@@ -1,9 +1,11 @@
 
 -- Creating the covid deaths table in data base
-CREATE TABLE public."covid-deaths"
+-- Creating the covid deaths table in data base
+CREATE TABLE public."covid_deaths"
 (
-    iso_code "char" NOT NULL,
-    continent character varying NOT NULL,
+    iso_code "char",
+    continent character varying,
+	location VARCHAR, 
     date date,
     population numeric,
     total_cases numeric,
@@ -32,5 +34,52 @@ WITH (
     OIDS = FALSE
 );
 
-ALTER TABLE public."covid-deaths"
+ALTER TABLE public."covid_deaths"
+    OWNER to postgres;
+-- Creating table for covid vaccinations
+CREATE TABLE public.covid_vaccinations
+(
+    iso_code VARCHAR,
+    continent VARCHAR,
+    location VARCHAR,
+    date date,
+    new_tests numeric,
+    total_tests numeric,
+    total_tests_per_thousand numeric,
+    new_tests_per_thousand numeric,
+    new_tests_smoothed numeric,
+    new_tests_smoothed_per_thousand numeric,
+    positive_rate numeric,
+    tests_per_case numeric,
+    tests_units VARCHAR,
+    total_vaccinations numeric,
+    people_vaccinated numeric,
+    people_fully_vaccinated numeric,
+    new_vaccinations numeric,
+    new_vaccinations_smoothed numeric,
+    total_vaccinations_per_hundred numeric,
+	people_vaccinated_per_hundred numeric,
+    people_fully_vaccinated_per_hundred numeric,
+    new_vaccinations_smoothed_per_million numeric,
+    stringency_index numeric,
+    population_density numeric,
+    median_age numeric,
+    aged_65_older numeric,
+    aged_70_older numeric,
+    gdp_per_capita numeric,
+    extreme_poverty numeric,
+    cardiovasc_death_rate numeric,
+    diabetes_prevalence numeric,
+    female_smokers numeric,
+    male_smokers numeric,
+    handwashing_facilities numeric,
+    hospital_beds_per_thousand numeric,
+    life_expectancy numeric,
+    human_development_index numeric
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public."covid_vaccinations"
     OWNER to postgres;
--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
 --Checking the data was properly imported into postgres
 SELECT * 
 FROM covid_deaths;
+
 --Checking the data was properly imported into postgres
 SELECT * 
 FROM covid_vaccinations;
